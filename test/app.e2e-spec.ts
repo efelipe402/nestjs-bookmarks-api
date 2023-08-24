72188976f0b186fdd21f49e743b0b99037d8e571
Andres Moncada <directorti@nextramo.com>
2023-08-23 23:29:15 -0500

    adding e2e testing for bookmarks

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -14,6 +14,10 @@ import { ROUTES_API } from '../src/shared/apiRoutes';
 import { ENDPOINTS } from '../src/shared/endpoints';
 import { AuthDto } from '../src/auth/dto';
 import { EditUserDto } from '../src/user/dto';
+import {
+  CreateBookmarkDto,
+  EditBookmarkDto,
+} from '../src/bookmarks/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -166,6 +170,128 @@ describe('App e2e', () => {
           .expectStatus(200)
           .expectBodyContains(dto.firstName)
           .expectBodyContains(dto.email);
+      });
+    });
+
+    describe('Bookmarks', () => {
+      describe('Get empty bookmarks', () => {
+        it('should get bookmarks', () => {
+          return pactum
+            .spec()
+            .get(`/${ROUTES_API.bookmarks}`)
+            .withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            })
+            .expectStatus(200)
+            .expectBody([]);
+        });
+      });
+
+      describe('Create bookmark', () => {
+        const dto: CreateBookmarkDto = {
+          title: 'First Bookmark',
+          link: 'https://www.youtube.com/watch?v=d6WC5n9G_sM',
+        };
+        it('should create bookmark', () => {
+          return pactum
+            .spec()
+            .post(`/${ROUTES_API.bookmarks}`)
+            .withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            })
+            .withBody(dto)
+            .expectStatus(201)
+            .stores('bookmarkId', 'id');
+        });
+      });
+
+      describe('Get bookmarks', () => {
+        it('should get bookmarks', () => {
+          return pactum
+            .spec()
+            .get(`/${ROUTES_API.bookmarks}`)
+            .withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            })
+            .expectStatus(200)
+            .expectJsonLength(1);
+        });
+      });
+
+      describe('Get bookmark by id', () => {
+        it('should get bookmark by id', () => {
+          return pactum
+            .spec()
+            .get(
+              `/${ROUTES_API.bookmarks}/{${ENDPOINTS.id}}`,
+            )
+            .withPathParams(
+              'id',
+              '$S{bookmarkId}',
+            )
+            .withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            })
+            .expectStatus(200)
+            .expectBodyContains('$S{bookmarkId}')
+            .inspect();
+        });
+      });
+
+      describe('Edit bookmark by id', () => {
+        const dto: EditBookmarkDto = {
+          title:
+            'Kubernetes Course - Full Beginners Tutorial (Containerize Your Apps!)',
+          description:
+            'Learn how to use Kubernetes in this complete course. Kubernetes makes it possible to containerize applications and simplifies app deployment to production.',
+        };
+        it('should edit bookmark', () => {
+          return pactum
+            .spec()
+            .patch(
+              `/${ROUTES_API.bookmarks}/{${ENDPOINTS.id}}`,
+            )
+            .withPathParams(
+              'id',
+              '$S{bookmarkId}',
+            )
+            .withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            })
+            .withBody(dto)
+            .expectStatus(200)
+            .expectBodyContains(dto.title)
+            .expectBodyContains(dto.description);
+        });
+      });
+
+      describe('Delete bookmark by id', () => {
+        it('should delete bookmark', () => {
+          return pactum
+            .spec()
+            .delete(
+              `/${ROUTES_API.bookmarks}/{${ENDPOINTS.id}}`,
+            )
+            .withPathParams(
+              'id',
+              '$S{bookmarkId}',
+            )
+            .withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            })
+            .expectStatus(204);
+        });
+
+        it('should get empty bookmarks', () => {
+          return pactum
+            .spec()
+            .get(`/${ROUTES_API.bookmarks}`)
+            .withHeaders({
+              Authorization: 'Bearer $S{userAt}',
+            })
+            .expectStatus(200)
+            .expectJsonLength(0);
+        });
       });
     });
   });
